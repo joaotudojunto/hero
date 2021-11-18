f98a8106f332c7941511aef9173c4a7e1d06498d
Joao Duarte <joaoduart@live.com.pt>
2021-11-18 09:49:58 +0000

    Terminal a funcionar, continua a passar paredes

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -6,26 +6,50 @@ import com.googlecode.lanterna.screen.Screen;
 import java.io.IOException;
 
 public class Arena {
-
+    Hero hero;
     public static int height, width;
-    private static Arena arena;
 
-    private static Screen screen;
 
-    public Arena(int height, int width){
+    public Arena(int height, int width) {
+        hero = new Hero(10, 10);
         this.height = height;
         this.width = width;
-        Hero hero = new Hero(10,10);
+
     }
 
-    public static void processKey(KeyStroke key) {
-        arena.processKey(key);
+    public void processKey(KeyStroke key) {
+
+        if (key.getKeyType() == KeyType.ArrowUp) {
+            moveHero(hero.moveUp());
+        } else if (key.getKeyType() == KeyType.ArrowDown) {
+            moveHero(hero.moveDown());
+        } else if (key.getKeyType() == KeyType.ArrowLeft) {
+            moveHero(hero.moveLeft());
+        } else {
+            moveHero(hero.moveRight());
+        }
+
     }
 
-    void draw(Screen screen) throws IOException {
-        Arena.screen.clear();
-        arena.draw(Arena.screen);
-        Arena.screen.refresh();
+    public void draw(Screen screen) throws IOException {
+        screen.clear();
+        hero.draw(screen);
+        screen.refresh();
+
+    }
+
+    private void moveHero(Position position) {
+        if (canHeroMove(position)) ;
+             hero.setPosition(position);
+    }
+
+
+    public static boolean canHeroMove(Position position) {
+        if (position.getX() >= 0 && position.getX() <= width && position.getY() >= 0 && position.getY() <= height) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -12,12 +12,9 @@ import java.io.IOException;
 public class Game{
 
     private static Screen screen;
-    private static Hero hero;
     private static int y,x;
     static Arena arena = new Arena(40,20);
     public int height, width;
-
-
 
 
     // construtor de game
@@ -28,8 +25,6 @@ public class Game{
             TerminalSize terminalSize = new TerminalSize(40, 20);
             DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
             Terminal terminal = terminalFactory.createTerminal();
-
-            hero = new Hero(10, 10);
 
             screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null); // we don't need acursor
@@ -43,28 +38,15 @@ public class Game{
 
     }
 
+    private void processKey(KeyStroke key) {
+        arena.processKey(key);
+    }
 
     private void draw() throws IOException {
         screen.clear();
-        hero.draw(screen);
+        arena.draw(screen);
         screen.refresh();
-
     }
-
-
-    void processKey(KeyStroke key) {
-
-        if(key.getKeyType() == KeyType.ArrowUp)
-            moveHero(hero.moveUp());
-        else if (key.getKeyType() == KeyType.ArrowDown)
-            moveHero(hero.moveDown());
-        else if (key.getKeyType() == KeyType.ArrowLeft)
-            moveHero(hero.moveLeft());
-        else
-            moveHero(hero.moveRight());
-    }
-
-
 
     public void run() throws IOException {
 
@@ -86,30 +68,4 @@ public class Game{
 
     }
 
-    public boolean canHeroMove(Position position) {
-        if (position.getX() >= 0 && position.getX() <= width && position.getY() >= 0 && position.getY() <= height){
-            return true;
-    }
-        return false;
-    }
-
-/*
-    public static boolean canHeroMove(Position position){
-        if(position.getX() >= 0 && position.getX() <= arena.width && position.getY() >= 0 && position.getY() <= arena.height) return false;
-        return true;
-    }
-*/
-
-    private void moveHero(Position position) {
-        if(canHeroMove(position));
-        //System.out.println(position.getX());
-        hero.setPosition(position);
-    }
-
-
-
 }
-
-
-
-
